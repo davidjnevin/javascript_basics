0db77b4450759e9e7adca095b663bea8cf82fe8b
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-06-05 13:15:59 +0200

    Add counter

---
+++ javascript-basic-projects/02-counter/setup/app.js
@@ -0,0 +1,31 @@
+// set inital count
+let count = 0
+
+// select vlaue and buttons
+const value = document.querySelector("#value"); // id = "value"
+const btns = document.querySelectorAll(".btn"); // class = 'btn'
+
+btns.forEach(function(btn) {
+	btn.addEventListener('click', function(e) {
+		const styles = e.currentTarget.classList;
+		if (styles.contains('decrease')) {
+			count--;
+		}
+		else if (styles.contains('reset')) {
+			count = 0;
+		}
+		else if (styles.contains('increase')) {
+			count++
+		}
+		if (count > 0) {
+			value.style.color = 'green';
+		}
+		if (count < 0) {
+			value.style.color = 'red';
+		}
+		if (count === 0) {
+			value.style.color = 'black';
+		}
+		value.textContent = count;
+	})
+});
