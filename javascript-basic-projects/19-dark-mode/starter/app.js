aaba2fa2500fe48809a46becba97cdbf1a6a2329
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-06-11 19:17:40 +0200

    Add  dark mode project

--- javascript-basic-projects/19-dark-mode/starter/app.js
+++ javascript-basic-projects/19-dark-mode/starter/app.js
@@ -1,1 +1,27 @@
-console.log('dark mode');
+
+const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
+
+toggleBtn.addEventListener('click', () => {
+	document.documentElement.classList.toggle('dark-theme');
+});
+
+const articlesData = articles.map((article) => {
+	const { title, date, snippet, length } = article;
+	// format date
+	const formatDate = moment(date).format('MMMM Do, YYYY');
+	return `<article class="post">
+<h2>${title}</h2>
+					<div class="post-info">
+<span>${formatDate}</span>
+<span>${length} min read</span>
+						<!-- <span>likes</span> -->
+					</div>
+					<p>
+${snippet}
+					</p>
+				</article>
+	`;
+}).join('');
+
+articlesContainer.innerHTML = articlesData;
