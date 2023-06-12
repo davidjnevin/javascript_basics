010e59b4f39d693351f482fd57a66ba95fdc2e00
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-06-13 00:52:52 +0200

    Add promises tutorial

---
+++ promises/async/promise-example.js
@@ -0,0 +1,11 @@
+// callbacks, promises, async/await
+// PROMISES - Pending, Resolved, Rejected
+// then catch - pass another callback
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.two');
+const heading3 = document.querySelector('.three');
+const container = document.querySelector('.img-container');
+const btn = document.querySelector('.btn');
+const url = 'https://images.unsplash.com/photo-1685644072593-b0e42d185d03?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4NjU4OTc1OA&ixlib=rb-4.0.3&q=80&w=1080';
+btn.addEventListener('click', () => { });
+
