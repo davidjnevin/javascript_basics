134d76660a0d11c23338cfc7f436f3dbebf5cdb1
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-06-17 12:06:31 +0200

    Add cocktails project

--- javascript-basic-projects/24-cocktails/setup/src/displayDrinks.js
+++ javascript-basic-projects/24-cocktails/setup/src/displayDrinks.js
@@ -2,28 +2,29 @@ import get from "./getElement.js";
 import { hideLoading } from "./toggleLoading.js";
 
 const displayDrinks = (drinks) => {
-  hideLoading();
-	const section = get(".section-center");
+  const section = get(".section-center");
   const title = get(".title");
   if (!drinks) {
+    // hide loading
+    hideLoading();
     title.textContent = "sorry, no drinks matched your search";
     section.innerHTML = null;
     return;
   }
-
   const newDrinks = drinks
     .map((drink) => {
       const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
-      return `<!-- single cocktail -->
-			<a href = "./drink.html?id=${id}" >
-			<article class="cocktail" data-id="${id}">
-			<img src="${image}" alt="${name}" />
-			<h3>${name}</h3></a>
-			</article>`;
+
+      return `<a href="drink.html">
+          <article class="cocktail" data-id="${id}">
+            <img src="${image}" alt="${name}" />
+            <h3>${name}</h3>
+          </article>
+        </a>`;
     })
     .join("");
-	hideLoading();
-	title.textContent = '';
+  hideLoading();
+  title.textContent = "";
   section.innerHTML = newDrinks;
   return section;
 };
--- javascript-basic-projects/24-cocktails/setup/src/presentDrinks.js
+++ javascript-basic-projects/24-cocktails/setup/src/presentDrinks.js
@@ -1,11 +1,17 @@
 import fetchDrinks from "./fetchDrinks.js";
 import displayDrinks from "./displayDrinks.js";
+import setDrink from "./setDrink.js";
 
 const showDrinks = async (URL) => {
-  // fetch drinks
-  const data = await fetchDrinks(URL);
-  // display drinks
-  const section = await displayDrinks(data);
+	// fetch drinks
+	const data = await fetchDrinks(URL);
+	// display drinks
+	const section = await displayDrinks(data);
+	if (section) {
+		setDrink(section);
+		// const title = section.querySelector(".title");
+		// title.textContent = "cocktails";
+	}
 };
 
 export default showDrinks;
