719cfd81db59e453322c2a3763e322cf7ee9ac54
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-06-18 01:07:33 +0200

    Add Vanilla JS storefront pt 1

--- javascript-basic-projects/29-comfy-store/starter/src/cart/toggleCart.js
+++ javascript-basic-projects/29-comfy-store/starter/src/cart/toggleCart.js
@@ -1,3 +1,17 @@
 import { getElement } from "../utils.js";
 
-export const openCart = () => {};
+const cartOverlay = getElement(".cart-overlay");
+const closeCartBtn = getElement(".cart-close");
+const toggleCartBtn = getElement(".toggle-cart");
+
+toggleCartBtn.addEventListener("click", () => {
+	cartOverlay.classList.add("show");
+});
+closeCartBtn.addEventListener("click", () => {
+	cartOverlay.classList.remove("show");
+});
+
+// Add produts to cart from product card page
+export const openCart = () => {
+	cartOverlay.classList.add("show");
+};
